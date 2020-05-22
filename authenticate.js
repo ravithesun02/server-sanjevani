1f65d76815b07d6e2c4f0c89ce4eee55e3eae7f9
Ravi Kumar <ravithesun02@gmail.com>
2020-05-22 23:53:15 +0530

    Fixed app

--- authenticate.js
+++ authenticate.js
@@ -3,14 +3,11 @@ var User=require('./models/users');
 var jwtStrategy=require('passport-jwt').Strategy;
 var ExtractJwt=require('passport-jwt').ExtractJwt;
 var jwt=require('jsonwebtoken');
-var LocalStrategy=require('passport-local').Strategy;
-var Admin=require('./models/admin');
+
 require('dotenv').config();
 
 
-exports.local = passport.use(new LocalStrategy(Admin.authenticate()));
-passport.serializeUser(Admin.serializeUser());
-passport.deserializeUser(Admin.deserializeUser());
+
 
 exports.getToken=function(user){
     return jwt.sign(user,process.env.SECRET_KEY,{expiresIn:'12000000 days'});
@@ -23,7 +20,7 @@ opts.jwtFromRequest=ExtractJwt.fromAuthHeaderAsBearerToken() ;
 opts.secretOrKey=process.env.SECRET_KEY;
 
 exports.jwtPassport=passport.use(new jwtStrategy(opts,(jwt_payload,done)=>{
-   // console.log(jwt_payload);
+   console.log(jwt_payload);
         User.findOne({_id:jwt_payload._id},(err,user)=>{  
             if(err)
             { 
@@ -33,26 +30,10 @@ exports.jwtPassport=passport.use(new jwtStrategy(opts,(jwt_payload,done)=>{
                 return done(null,user);
             else
                 return done(null,null);
-        });
-    
-   
-      
-    
- 
+        }); 
 }));
 
 exports.verifyUser=passport.authenticate('jwt',{session:false});
 
-exports.verifyMainAdmin=(req,res,next)=>{
-    if(req.user.admin_type==='DC')
-    {
-        next();
-    }
-    else
-    {
-        let err=new Error('You are not authorized to perform this operation !');
-        err.status=403;
-        next(err);
-    }
-}
+
 
